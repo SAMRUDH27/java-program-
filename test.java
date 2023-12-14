ab8b7c03d4f72b8c9bb5476eaa70e32a3f213007
SAMRUDH27 <samrudh.bangera@bcah.christnuniversity.in>
2023-12-14 12:36:34 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class stringarray
+{
+    public static void main(String[]args)
+{
+    Scanner scanner = new Scanner(System.in);
+    int array[]=new int[5];
+    for(int i=0;i<array.length;i++){
+        System.out.println("enter number of element"+(i+1));
+        array[i]=scanner.nextInt();
+    }
+    for(int j=0;j<array.length;j++){
+        System.out.println("element at"+(j+1)+":"+array[j]);
+    }
+scanner.close();
+}
+    }
